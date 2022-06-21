9416e2a8bae18ceb378af1575370ed23e16c30e5
elinebroier <elinebroier@hotmail.com>
2022-06-21 13:21:28 +0200

    added custom error pages

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,13 +1,41 @@
-<!doctype html>
-<html lang="en">
+{{--@extends('layout')--}}
+
+{{--@section('content')--}}
+
+{{--    <h1>Hallo dit is 500</h1>--}}
+
+{{--@endsection--}}
+    <!DOCTYPE html>
+<html>
 <head>
+
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type='text/css' href="css/main.css">
+
+    <ul class="nav">
+        <li class="nav"><a href="/welcome">Home</a></li>
+        <li class="nav"><a href="/hbo">HBO</a></li>
+        <li class="dropdown">
+            <a href="/profile" class="dropbt">Profile</a>
+            <div class="dropdown-content">
+                <a href="/photos">Photo's</a>
+            </div>
+        </li>
+        <li class="nav"><a href="/grades">Dashboard</a></li>
+        <li class="nav"><a href="/motivation">Motivation</a></li>
+        <li class="nav"><a href="/profession">Profession</a></li>
+        <li class="nav"><a href="/articles">Articles</a></li>
+        <li class="nav"><a href="/faq">FAQ</a></li>
+    </ul>
 </head>
+
 <body>
-<h1>Hallo dit is 500</h1>
+
+<div class="error" id="500">
+    <h1><b style="color: red">500: </b>something went wrong</h1>
+</div>
+
 </body>
 </html>
+
